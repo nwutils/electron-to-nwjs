0825ab59cb385db73152bc7826438da735037db2
VitorMM <vitor251093@gmail.com>
2022-08-29 21:34:28 -0300

    All HTML files should be considered when loading JS files

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,21 @@
 // NW.js 0.14.7 includes Chromium 50.0.2661.102 and Node.js 5.11.1
 // https://nodejs.org/dist/latest-v5.x/docs/api/
 
-const cheerio = require('cheerio');
 const fs = require('fs')
 const path = require('path')
+const cheerio = require('cheerio')
+const child_process = require('child_process')
 
 const projectPath = path.resolve(__dirname, '.', 'www')
-const projectPackagePath = path.resolve(projectPath, 'package.json')
-const projectPackageStr = fs.readFileSync(projectPackagePath, {encoding: 'utf-8'})
-const projectPackageJson = JSON.parse(projectPackageStr)
-const nwjsConfig = projectPackageJson.nwjs || {}
-const windowConfig = nwjsConfig.window || {}
-const indexHtml = windowConfig.index || "index.html"
-
-var indexHtmlContents = fs.readFileSync(path.join(projectPath, indexHtml), {encoding: 'utf-8'})
-const $ = cheerio.load(indexHtmlContents);
-var scripts = $('script[src]')
-const jsFiles = scripts.map(function() { return $(this).attr('src'); }).get()
+const listHtmlsStr = child_process.execSync('find . -type f -name "*.html"', {cwd: projectPath, encoding: 'utf8'})
+const listHtmls = listHtmlsStr.split("\n").filter(line => line.trim().length > 0 && !line.startsWith("./node_modules/"))
+const jsFiles = []
+listHtmls.forEach(htmlPath => {
+    const indexHtmlContents = fs.readFileSync(path.join(projectPath, htmlPath), {encoding: 'utf-8'})
+    const $ = cheerio.load(indexHtmlContents);
+    const scripts = $('script[src]')
+    jsFiles.push(...scripts.map(function() { return $(this).attr('src'); }).get())
+})
 
 module.exports = (env, argv) => ({
     target: ['nwjs', 'node5'],
