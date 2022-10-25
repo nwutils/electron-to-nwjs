c6c67ace27ff0dc428b45c8d3f0a67daec417151
VitorMM <vitor251093@gmail.com>
2022-10-25 16:27:29 -0300

    Updating dialog notes

--- fakelibs/electron/dialog/index.js
+++ fakelibs/electron/dialog/index.js
@@ -2,20 +2,10 @@
   Electron Docs
   https://www.electronjs.org/docs/latest/api/dialog
 
-  NW.js Docs
-  https://github.com/nwjs/nw.js/wiki/File-dialogs
-  https://github.com/nwjs/nw.js/issues/3430
-  https://docs.nwjs.io/en/latest/References/Changes%20to%20DOM/
-
   Display native system dialogs for opening and saving files, alerting, etc.
   Only available in the main process.
 
-  NW.js uses the browser methods to start dialogs, with <input type='file' />,
-  but it has some properties and events exclusive to NW.js, which make input file
-  a decent replacement for Electron's showOpenDialog and showSaveDialog, even
-  if it doesn't support all Electron's options.
-
-  The browser functions confirm() and alert() are also available.
+  The methods for calling dialogs will differ from system to system.
 */
 
 module.exports = (function(){
