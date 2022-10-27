6cab0698232b94325d9d263e0beeb92778b8a1e6
VitorMM <vitor251093@gmail.com>
2022-10-27 09:58:45 -0300

    Starting to implement net class using node-fetch

--- fakelibs/electron/net.js
+++ fakelibs/electron/net.js
@@ -1,9 +1,10 @@
 /*
   Electron Docs
   https://www.electronjs.org/docs/latest/api/net
+  https://www.electronjs.org/docs/latest/api/client-request
 
-  NW.js Docs
-  ?
+  node-fetch Docs
+  https://www.npmjs.com/package/node-fetch
 
   Issue HTTP/HTTPS requests using Chromium's native networking library.
   Only available in the main process.
@@ -12,6 +13,7 @@
 */
 
 const session = require('./session')
+const fetch = require('node-fetch')
 
 class ClientRequest {
     constructor(options) {
@@ -20,7 +22,7 @@ class ClientRequest {
         }
         this.method = options.method || "GET"
         this.session = options.session !== undefined ? options.session : (session.fromPartition(options.partition || ""))
-        // credentials
+        // credentials (include / omit)
         this.useSessionCookies = options.useSessionCookies || false
         this.redirect = options.redirect || "follow"
         
@@ -29,13 +31,72 @@ class ClientRequest {
         }
         else {
             let protocol = options.protocol || "http:"
-            let host = options.host || `${options.hostname}:${options.port}`
-            this.url = `${protocol}//${host}${options.path}`
+            let port = options.port || (protocol === "http:" ? 80 : (protocol === "https:" ? 443 : undefined))
+            if (options.host === undefined) {
+                if (options.hostname === undefined) {
+                    throw new Error("Invalid request: no hostname specified")
+                }
+                if (port === undefined) {
+                    throw new Error(`Invalid request: no port specified for protocol ${protocol}`)
+                }
+            }
+            let host = options.host || `${options.hostname}:${port}`
+            let path = options.path || "/"
+            this.url = `${protocol}//${host}${path}`
         }
+
+        const headers = {
+            // session
+            // credentials
+            // useSessionCookies
+        }
+
+        // that.dispatchEvent({type:'login', args:[...]})
+
+        let that = this
+        fetch(this.url, {
+            method: that.method,
+            headers: headers,
+            redirect: that.redirect,
+        }).then(response => {
+            that.response = response
+
+            that.dispatchEvent({type:'finish', args:[]})
+
+            if (response.redirected) {
+                const statusCode = response.status
+                const method = that.method
+                const redirectUrl = response.url
+                const responseHeaders = response.headers.raw()
+                that.dispatchEvent({type:'redirect', args:[statusCode, method, redirectUrl, responseHeaders]})
+            }
+
+            let setCookie = response.headers.raw()['set-cookie']
+            // that.dispatchEvent({type:'response', args:[response]})
+
+            that.dispatchEvent({type:'close', args:[]})
+        }).catch(error => {
+            if (error instanceof AbortError) {
+                that.dispatchEvent({type:'abort', args:[]})
+            }
+            else {
+                that.dispatchEvent({type:'error', args:[error]})
+            }
+        })
     }
 
-    on(eventName, callback) {
-        
+
+    _events = {}
+    dispatchEvent(event) {
+        let listeners = this._events[event.type] || [];
+        listeners.forEach(listener => {
+            listener.apply(undefined, event.args);
+        })
+    }
+    on(eventName, listener) {
+        this._events[eventName] = this._events[eventName] || []
+        this._events[eventName].push(listener);
+        return this;
     }
 }
 
