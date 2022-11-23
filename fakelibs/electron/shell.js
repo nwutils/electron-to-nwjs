22aaafca437c950417e0439970f98c13b4ea2eed
VitorMM <vitor251093@gmail.com>
2022-11-23 19:46:41 -0300

    Adding shell.readShortcutLink

--- fakelibs/electron/shell.js
+++ fakelibs/electron/shell.js
@@ -12,6 +12,21 @@
   using Node.
 */
 
+const getWindowsShortcutProperties = require('get-windows-shortcut-properties');
+
+class ShortcutDetails {
+    constructor({target, cwd, args, description, icon, iconIndex}) {
+        this.target = target
+        this.cwd = cwd
+        this.args = args
+        this.description = description
+        this.icon = icon
+        this.iconIndex = iconIndex
+        this.appUserModelId = undefined
+        this.toastActivatorClsid = undefined
+    }
+}
+
 const shell = {
     showItemInFolder(item) {
         nw.Shell.showItemInFolder(item)
@@ -28,8 +43,27 @@ const shell = {
     // trashItem(path)
     beep() {
         process.stdout.write('\x07')
-    }
+    },
     // writeShortcutLink(shortcutPath[, operation], options) (Windows only)
-    // readShortcutLink(shortcutPath) (Windows only)
+    readShortcutLink(shortcutPath) {
+        if (process.platform !== 'win32') {
+            throw new Error("readShortcutLink can only be used on Windows")
+        }
+        const output = getWindowsShortcutProperties.sync(shortcutPath);
+        if (!output) {
+            throw new Error("Unknown error while trying to read shortcut link")
+        }
+        let iconParts = output.IconLocation.split(",")
+        let iconIndex = parseInt(iconParts.pop())
+        let icon = iconParts.join(",")
+        return new ShortcutDetails({
+            target: output.TargetPath,
+            cwd: output.WorkingDirectory,
+            args: output.Arguments,
+            description: output.Description,
+            icon,
+            iconIndex
+        })
+    }
 }
 module.exports = shell
