6c39b7bfb871d184079d929cbe5931209767d02e
VitorMM <vitor251093@gmail.com>
2023-03-14 00:08:15 -0300

    Linux should support showMessageBoxSync

--- fakelibs/electron/dialog/linux.js
+++ fakelibs/electron/dialog/linux.js
@@ -13,8 +13,34 @@
 */
 
 const BaseDialog = require('./base')
+const child_process = require('child_process')
 
 class LinuxDialog extends BaseDialog {
-  
+  static showMessageBoxSync(window, opts) {
+    if (opts === undefined) {
+      opts = window
+    }
+    let {message, type, buttons, defaultId, title, detail, icon, textWidth, cancelId, noLink, normalizeAccessKeys} = opts
+    
+    if (!buttons && buttons.length === 0) {
+      buttons = ["OK"]
+    } else {
+      buttons = buttons.slice(0)
+    }
+
+    let messageStr = [(message || ""), (detail || "")].filter(l => l.length > 0).join("\n")
+    let args = ["zenity", "--info", "--title", (title || ""), "--text", messageStr, "--ok-label", buttons.shift()]
+    buttons.forEach(button => {
+      args.push("--extra-button")
+      args.push(button)
+    })
+    let cmd = args.shift()
+    var response = child_process.spawnSync(cmd, args, {encoding:'utf-8'})
+    if (response.status === 0) {
+      return 0
+    }
+    let selectedOption = response.stdout.trim()
+    return 1 + buttons.indexOf(selectedOption)
+  }
 }
 module.exports = LinuxDialog
