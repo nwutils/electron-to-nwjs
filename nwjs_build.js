df2d5c6f8e24580b2a5fc139db3314836a18e16b
VitorMM <vitor251093@gmail.com>
2023-03-23 02:02:06 -0300

    Fixing nwjs_build.js

--- nwjs_build.js
+++ nwjs_build.js
@@ -3,4 +3,9 @@ const NwBuilder = require('nw-builder');
 var nw = new NwBuilder(require('./nwjs_build_config.json'));
 
 nw.on('log', console.log);
-await nw.build()
+nw.build().then(function(){
+    console.log("Build finished")
+})
+.catch(function(error) {
+    console.error(error);
+});
