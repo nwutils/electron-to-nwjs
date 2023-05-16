ce78de8310472141dddb842cefe3db4fd0bc0b66
VitorMM <vitor251093@gmail.com>
2023-05-15 23:07:50 -0300

    Crash report implementation almost finishep

--- fakelibs/electron/crashReporter.js
+++ fakelibs/electron/crashReporter.js
@@ -12,75 +12,127 @@
 */
 
 const app = require('./app')
+const fs = require('fs')
+const path = require('path')
 
 class crashReporter {
-  static submitURL;
-  static productName = app.name;
-  static uploadToServer = true;
-  static ignoreSystemCrashHandler = false;
-  static rateLimit = false;
-  static extra = {}
-  static globalExtra = {}
+  _submitURL;
+  _productName = app.name;
+  _uploadToServer = true;
+  _ignoreSystemCrashHandler = false;
+  _rateLimit = false;
+  _extra = {}
+  _globalExtra = {}
 
-  static get companyName() {
-    return this.globalExtra?._companyName
+  get _companyName() {
+    return this._globalExtra?._companyName
   }
-  static set companyName(companyName) {
-    this.globalExtra = this.globalExtra || {}
-    this.globalExtra._companyName = companyName
-  }
-
-  static start(options) {
-    this.submitURL = options.submitURL || this.submitURL
-    this.productName = options.productName || this.productName
-    this.companyName = options.companyName || this.companyName
-    this.uploadToServer = options.uploadToServer || this.uploadToServer
-    this.ignoreSystemCrashHandler = options.ignoreSystemCrashHandler || this.ignoreSystemCrashHandler
-    this.rateLimit = options.rateLimit || this.rateLimit
-    this.extra = Object.assign({}, this.extra, options.extra);
-    this.globalExtra = Object.assign({}, this.globalExtra, options.globalExtra);
+  set _companyName(companyName) {
+    this._globalExtra = this._globalExtra || {}
+    this._globalExtra._companyName = companyName
   }
 
-  static getUploadToServer() {
-    return this.uploadToServer
-  }
-  static setUploadToServer(uploadToServer) {
-    this.uploadToServer = uploadToServer || this.uploadToServer
-  }
-  static addExtraParameter(key, value) {
-    this.extra[key] = value
-  }
-  static removeExtraParameter(key) {
-    delete this.extra[key]
-  }
-  static getParameters() {
-    return this.extra
+  start(options) {
+    this._submitURL = options.submitURL || this._submitURL
+    this._productName = options.productName || this._productName
+    this._companyName = options.companyName || this._companyName
+    this._uploadToServer = options.uploadToServer || this._uploadToServer
+    this._ignoreSystemCrashHandler = options.ignoreSystemCrashHandler || this._ignoreSystemCrashHandler
+    this._rateLimit = options.rateLimit || this._rateLimit
+    this._extra = Object.assign({}, this._extra, options.extra);
+    this._globalExtra = Object.assign({}, this._globalExtra, options.globalExtra);
   }
 
-  static _minidumpFolder() {
+  getUploadToServer() {
+    return this._uploadToServer
+  }
+  setUploadToServer(uploadToServer) {
+    this._uploadToServer = uploadToServer || this._uploadToServer
+  }
+  addExtraParameter(key, value) {
+    this._extra[key] = value
+  }
+  removeExtraParameter(key) {
+    delete this._extra[key]
+  }
+  getParameters() {
+    return this._extra
+  }
+
+  _minidumpFolder() {
     return app.getPath("crashDumps")
   }
-  static _buildCrashReportPayload(process_type, file) {
+  _buildCrashReportPayload(dmpFilePath) {
     let payload = {
       ver: __electron_version,
       platform: process.platform,
-      process_type: process_type,
+      process_type: __nwjs_is_main ? "main" : "renderer",
       // guid string - e.g. '5e1286fc-da97-479e-918b-6bfb0c3d1c72'. machine unique identifier
       _version: __nwjs_app_version,
-      _productName: this.productName,
+      _productName: this._productName,
       prod: "Electron",
-      _companyName: this.companyName,
-      upload_file_minidump: file
+      _companyName: this._companyName,
+      upload_file_minidump: fs.createReadStream(dmpFilePath)
     }
-    Object.assign(payload, this.extra)
+    Object.assign(payload, this._extra)
     return payload
   }
+  _onReady() {
+    let submitURL = this._submitURL
+    if (!submitURL) {
+      return
+    }
+    let minidumpFolder = this._minidumpFolder()
+    let pendingDumpsFolder = path.join(minidumpFolder, "completed")
+    let completedDumpsFolder = path.join(minidumpFolder, "completed_electron_to_nwjs")
 
-  static getLastCrashReport() {
-    return null
+    if (!fs.existsSync(completedDumpsFolder)) {
+      fs.mkdirSync(completedDumpsFolder)
+    }
+
+    let uuids = fs.readdirSync(pendingDumpsFolder).filter(file => file.endsWith(".dmp"))
+                  .map(file => file.substring(0, file.length - ".dmp".length))
+    for (let uuid of uuids) {
+      let dmpOriginalFilePath  = path.join(pendingDumpsFolder, uuid + ".dmp")
+      let metaOriginalFilePath = path.join(pendingDumpsFolder, uuid + ".meta")
+      let dmpFinalFilePath  = path.join(pendingDumpsFolder, uuid + ".dmp")
+      let metaFinalFilePath = path.join(pendingDumpsFolder, uuid + ".meta")
+
+      // TODO: Retrieve app name and version from meta file
+
+      var xhr = new XMLHttpRequest();
+      const form = new FormData();
+      const options = this._buildCrashReportPayload(dmpOriginalFilePath)
+      Object.keys(options).forEach(optKey => form.append(optKey, options[optKey]))
+      xhr.onload = function() {
+        fs.renameSync(dmpOriginalFilePath, dmpFinalFilePath)
+        fs.renameSync(metaOriginalFilePath, metaFinalFilePath)
+      }
+      xhr.open("post", submitURL);
+      xhr.send(form);
+    }
   }
-  static getUploadedReports() {
-    return []
+
+  getLastCrashReport() {
+    return this.getUploadedReports().reduce((a, b) => a.date.getTime() > b.date.getTime() ? a : b)
+  }
+  getUploadedReports() {
+    let completedDumpsFolder = path.join(minidumpFolder, "completed_electron_to_nwjs")
+    let files = fs.readdirSync(completedDumpsFolder).filter(file => file.endsWith(".meta"))
+    let reports = files.map(file => {
+      let id = file.substring(0, file.length - ".meta".length)
+      let filePath = path.join(completedDumpsFolder, file)
+      let date = new Date() // Load date from filePath
+      return {id, date}
+    })
+    return reports
   }
 }
-module.exports = crashReporter
+
+let crashReporterInstance = new crashReporter()
+
+app.on('ready', function(){
+  crashReporterInstance._onReady()
+})
+
+module.exports = crashReporterInstance
